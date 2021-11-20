ffe5b646810ace537d0c3d8dc61cff72ebebd3eb
Carlos Pasten <carlos92.pastenbravo@gmail.com>
2021-11-19 21:40:11 -0300

    clase 5

--- assets/index.js
+++ assets/index.js
@@ -1,32 +1,12 @@
+import MediaPlayer from './MediaPlayer.js'
+
 const video = document.querySelector('.movie');
-        const button = document.querySelector('button');
+const button = document.querySelector('button');
 
         
-        class MediaPlayer {
-            
-            constructor(video){
-                this.video = video
-            }
-
-            play(){
-                this.video.play()
-            }
-
-            pause(){
-                this.video.pause()
-            }
-
-            togglePlay(){
-                if(this.video.paused){
-                    this.video.play()
-                }else{
-                    this.video.pause()
-                }
-            }
-
-        }
 
 
-        const player = new MediaPlayer(video);
 
-        button.onclick = () => player.togglePlay();
+const player = new MediaPlayer(video);
+
+button.onclick = () => player.togglePlay();
