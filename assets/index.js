1e8c5f820ee6967d10ea54ada8ab783dfd968656
Carlos Pasten <carlos92.pastenbravo@gmail.com>
2021-11-19 23:25:20 -0300

    clase 7

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,17 @@
-import MediaPlayer from './MediaPlayer.js'
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
-const video = document.querySelector('.movie');
-const button = document.querySelector('button');
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-        
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
 
-
-
-const player = new MediaPlayer(video);
-
-button.onclick = () => player.togglePlay();
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
