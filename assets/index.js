d6003f9ab2ed81c7e5ee02309ef4bf2622a81228
Carlos Pasten <carlos92.pastenbravo@gmail.com>
2021-11-22 21:15:42 -0300

    clase 19

--- assets/index.js
+++ assets/index.js
@@ -6,7 +6,7 @@ const player = new MediaPlayer(
           {
             el: video,
             plugins: [
-              // new AutoPlay()
+               new AutoPlay()
             ] 
           });
 
--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -1,6 +1,11 @@
 function AutoPlay() {}
 AutoPlay.prototype.run = function(player) {
-  player.mute();
+  if(!player.muted){
+    player.muted = true;
+  }else{
+    player.muted = false;
+  }
+  
   player.play();
 };
 
